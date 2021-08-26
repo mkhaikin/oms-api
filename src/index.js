11e9f9b487ef0408aafaaa0e9d6c98f26a1de924
yefimkh <michael.khaikin@gmail.com>
2021-08-26 14:47:46 -0400

    Changed html

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const app = express();
 
 app.use(cors({
     credentials: true,
-    origin: process.env.CLIENT_URL
-    
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 app.use(express.json());
 app.use(cookieParser());
