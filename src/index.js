3942ae768294cc23ac92340849a4975cbf184027
yefimkh <michael.khaikin@gmail.com>
2021-09-09 21:02:45 -0400

    cors credentials and origin commented

--- src/index.js
+++ src/index.js
@@ -8,12 +8,14 @@ const errorMiddleware = require('./middleware/error-middleware')
 
 
 const app = express();
-
+/*
 app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"]
 }));
+*/
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
