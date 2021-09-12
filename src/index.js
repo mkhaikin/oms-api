db689195919ca8856209aabd14aa4e6b882d7b85
yefimkh <michael.khaikin@gmail.com>
2021-09-12 15:54:10 -0400

    clear credentials

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 */
-app.use(cors({credentials: true,}));
+//app.use(cors({credentials: true,}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
