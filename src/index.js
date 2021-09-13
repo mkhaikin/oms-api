a6e25a99b16eb0d54f9e46ced50df330d3b56aa6
yefimkh <michael.khaikin@gmail.com>
2021-09-13 14:04:39 -0400

    set origin to netlify app

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const errorMiddleware = require('./middleware/error-middleware')
 const app = express();
 /* */
 app.use(cors({
-    origin: '*',
+    origin: 'https://keen-brown-026739.netlify.app/',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
